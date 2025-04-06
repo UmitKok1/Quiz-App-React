c22d869939d9ac30dfb8b4c682726c513454990a
UmitKok21 <umit.kok21@gmail.com>
2025-04-06 20:12:33 +0300

    Quiz logic added.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,22 +1,33 @@
 import { useState } from "react"
 import questions from "../questions";
+import quizCompleteImg from '../assets/quiz-complete.png'
 export default function Quiz() {
 
     const [userAnswers, setUserAnswers] = useState([]);
     const activeQuestionIndex = userAnswers.length;
+    const quizIsComplete = activeQuestionIndex === questions.length;
 
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((previousUserAnswers) => {
             return [...previousUserAnswers, selectedAnswer];
         });
     }
+    if (quizIsComplete) {
+        return <div id="summary">
+            <img src={quizCompleteImg} />
+            <h2>Quiz Completed!</h2>
+        </div>
+    }
+
+    const shuffledAnswers = [...questions[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
 
     return (
         <div id="quiz">
             <div id="question">
                 <h2>{questions[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {questions[activeQuestionIndex].answers.map(answer => (
+                    {shuffledAnswers.map(answer => (
                         <li key={answer} className="answer">
                             <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                         </li>
